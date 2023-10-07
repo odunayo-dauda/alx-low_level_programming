69e9a9ad910276a05166a222880d18c2cf603dd6
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-07 07:32:28 -0700

    commit of commit#

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

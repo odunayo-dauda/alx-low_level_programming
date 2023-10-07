66f2551d93f328919063cd77954a614ed6263dc9
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-07 06:57:11 -0700

    2nd fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }

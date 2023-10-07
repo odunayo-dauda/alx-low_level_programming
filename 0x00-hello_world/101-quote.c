98d947169326bfec87d549729fdd74421c7a73ac
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-07 08:40:54 -0700

    FIX fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, quote, 59);
 return (1);
 }

436e12b2ee2167ad0d35c1a86f1e19d848c44b37
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-14 10:05:17 -0700

    void fizz_buzz(void) commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - Prints Fizz, Buzz, or FizzBuzz for multiples of 3 and 5.
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+}

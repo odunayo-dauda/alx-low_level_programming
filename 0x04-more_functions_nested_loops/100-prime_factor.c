c2ade7008b3af09431f7bcf15feccf85a44c409f
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-22 01:26:58 -0700

    more nested task-11 recommit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,67 +1,47 @@
 #include "main.h"
 
 /**
- * is_prime - Checks if a number is prime
- * @n: The number to check
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(long n)
-{
-	if (n <= 1)
-		return (0);
-	if (n <= 3)
-		return (1);
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	for (long i = 5; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-	}
-
-	return (1);
-}
-
-/**
  * largest_prime_factor - Finds the largest prime factor of a number
- * @n: The number to find the largest prime factor of
+ * @n: The number to find the largest prime factor for
  * Return: The largest prime factor
  */
 long largest_prime_factor(long n)
 {
-	long largest = -1;
+	long largest_prime = -1;
 
 	while (n % 2 == 0)
 	{
-		largest = 2;
+		largest_prime = 2;
 		n /= 2;
 	}
 
-	for (long i = 3; i <= n; i += 2)
+	for (long i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
 		{
-			if (is_prime(i))
-				largest = i;
+			largest_prime = i;
 			n /= i;
 		}
 	}
+	if (n > 2)
+	{
+		largest_prime = n;
+	}
 
-	return (largest);
+	return (largest_prime);
 }
 
 /**
  * main - Entry point to app
  * Return: The success
  */
+
 int main(void)
 {
-	long number = 612852475143;
-	long largest_prime = largest_prime_factor(number);
+	long n = 612852475143;
+	long result = largest_prime_factor(n);
 
-	_putchar(largest_prime + '0');
+	_putchar(result + '0');
 	_putchar('\n');
-
 	return (0);
 }

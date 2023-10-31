df412b33e16c02420ade1bfca539bfd13e878f04
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-31 15:50:54 -0700

    first task hard pointer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: A pointer to the string to convert.
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] == ' ')
+		i++;
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}

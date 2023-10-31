104d2b8a8d944030aaeb0b3e9e75340c52685ba8
oduayo-dauda <daudavictorodunayo@gmail.com>
2023-10-31 15:38:38 -0700

    _putchar palava

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,30 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar("%d", a[i]);
+		int num = a[i];
+		int divisor = 1;
+
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+
+		while (num / divisor >= 10)
+			divisor *= 10;
+
+		while (divisor > 0)
+		{
+			_putchar((num / divisor) + '0');
+			num %= divisor;
+			divisor /= 10;
+		}
+
 		if (i < n - 1)
-			_putchar(", ");
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
